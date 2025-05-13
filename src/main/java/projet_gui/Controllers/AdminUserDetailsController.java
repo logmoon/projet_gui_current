b3a86b72536adaff0f87d11363683e5e2513f5d6
Amen <53493383+logmoon@users.noreply.github.com>
2025-05-13 02:37:37 +0100

    updated admin stuff

--- src/main/java/projet_gui/Controllers/AdminUserDetailsController.java
+++ src/main/java/projet_gui/Controllers/AdminUserDetailsController.java
@@ -1,11 +1,7 @@
 package projet_gui.Controllers;
 
-import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
 import projet_gui.App;
 import projet_gui.Entities.Utilisateur;
 import projet_gui.Services.AuthService;
@@ -25,36 +21,6 @@ public class AdminUserDetailsController extends ControllerBaseWithSidebar {
 
     @FXML
     private Label statusLabel;
-
-    @FXML
-    private TableView<?> fieldsTable;
-
-    @FXML
-    private TableColumn<?, Integer> fieldIdColumn;
-
-    @FXML
-    private TableColumn<?, String> fieldNameColumn;
-
-    @FXML
-    private TableColumn<?, String> fieldLocationColumn;
-
-    @FXML
-    private TableColumn<?, Double> fieldSizeColumn;
-
-    @FXML
-    private TableView<?> cropsTable;
-
-    @FXML
-    private TableColumn<?, Integer> cropIdColumn;
-
-    @FXML
-    private TableColumn<?, String> cropNameColumn;
-
-    @FXML
-    private TableColumn<?, String> cropFieldColumn;
-
-    @FXML
-    private TableColumn<?, String> cropStatusColumn;
 
     private UtilisateurService utilisateurService;
     private Utilisateur selectedUser;
@@ -80,8 +46,6 @@ public class AdminUserDetailsController extends ControllerBaseWithSidebar {
         }
         
         loadUserDetails(selectedUserId);
-        setupTables();
-        loadUserData();
     }
 
     private void loadUserDetails(int userId) {
@@ -96,30 +60,6 @@ public class AdminUserDetailsController extends ControllerBaseWithSidebar {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-    }
-
-    private void setupTables() {
-        // Setup fields table
-        fieldIdColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
-        fieldNameColumn.setCellValueFactory(new PropertyValueFactory<>("nom"));
-        fieldLocationColumn.setCellValueFactory(new PropertyValueFactory<>("location"));
-        fieldSizeColumn.setCellValueFactory(new PropertyValueFactory<>("superficie"));
-        
-        // Setup crops table
-        cropIdColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
-        cropNameColumn.setCellValueFactory(new PropertyValueFactory<>("nom"));
-        cropFieldColumn.setCellValueFactory(new PropertyValueFactory<>("field"));
-        cropStatusColumn.setCellValueFactory(new PropertyValueFactory<>("status"));
-    }
-
-    private void loadUserData() {
-        if (selectedUser == null) return;
-        
-        // TODO: Load fields and crops for the selected user
-        // This will be implemented when the corresponding services are available
-        // For now, we'll just show empty tables
-        fieldsTable.setItems(FXCollections.observableArrayList());
-        cropsTable.setItems(FXCollections.observableArrayList());
     }
 
     @FXML
